3dc8d347789889cc91236f6f9e7eac2a79c3026e
Kirill <krisstal.mail@gmail.com>
2015-04-11 23:30:28 +0500

    improve example

--- README.md
+++ README.md
@@ -1,7 +1,23 @@
 # ForceAtlas2 layout algorithm for ngraph
 
-It's [sigma.js plugin](https://github.com/jacomyal/sigma.js/tree/master/plugins/sigma.layout.forceAtlas2) adapted for 
-performing [ngraph](https://github.com/anvaka/ngraph.graph) layout
+It's [sigma.js plugin][1] adapted for 
+performing [ngraph][2] layout
+
+<a href="https://update-crystal.codio.io/images/100-10000.PNG" title="layout example 10k nodes, 100 communities">
+  <img alt="layout example 10k nodes, 100 communities" src="https://update-crystal.codio.io/images/100-10000.PNG" width="150"/>
+</a>
+<a href="https://update-crystal.codio.io/images/2-4000(1000).PNG" title="layout example 4k nodes, 2 communities">
+  <img alt="layout example 4k nodes, 2 communities" src="https://update-crystal.codio.io/images/2-4000(1000).PNG" width="150"/>
+</a>
+<a href="https://update-crystal.codio.io/images/2-4000(500).PNG" title="layout example 4k nodes, 2 communities">
+  <img alt="layout example 4k nodes, 2 communities" src="https://update-crystal.codio.io/images/2-4000(500).PNG" width="150"/>
+</a>
+<a href="https://update-crystal.codio.io/images/30-4000.PNG" title="layout example 4k nodes, 30 communities">
+  <img alt="layout example 4k nodes, 30 communities" src="https://update-crystal.codio.io/images/30-4000.PNG" width="150"/>
+</a>
+<a href="https://update-crystal.codio.io/images/5-1000.PNG" title="layout example 1k nodes, 5 communities">
+  <img alt="layout example 1k nodes, 5 communities" src="https://update-crystal.codio.io/images/5-1000.PNG" width="150"/>
+</a>
 
 ##Usage
 
@@ -15,3 +31,6 @@ npm i gulp -g && npm i && gulp watch_browserify
 ```
 ## TODO
 - handle graph changes 
+
+[1]: https://github.com/jacomyal/sigma.js/tree/master/plugins/sigma.layout.forceAtlas2
+[2]: https://github.com/anvaka/ngraph.graph
--- example/index.js
+++ example/index.js
@@ -2,54 +2,99 @@ var Viva = require('./node_modules/vivagraphjs/src/viva');
 var graphGenerator = Viva.Graph.generator();
 var graph = graphGenerator.grid(20, 20);
 var ForceAtlas2 = require('../Layout');
+var agm = require('ngraph.agmgen');
+var _ = require('underscore');
+var colors = require('./colors');
+var rendererSstate = null;
 
 function onLoad() {
-    var layout = ForceAtlas2(graph, {
-        gravity: 1,
-        linLogMode: false,
-        strongGravityMode: false,
-        slowDown: 0.5,
-        outboundAttractionDistribution: false,
-        iterationsPerRender: 1,
-        barnesHutOptimize: false,
-        barnesHutTheta: 0.5,
-        worker: true
-    });
 
-/*    var layout = Viva.Graph.Layout.forceDirected(graph, {
-        springLength : 30,
-        springCoeff : 0.0008,
-        dragCoeff : 0.01,
-        gravity : -1.2,
-        theta : 1
-    });*/
+    document.querySelector('#setup').onsubmit = function (e) {
+        if (rendererSstate) rendererSstate.dispose();
 
-    var graphics = Viva.Graph.View.webglGraphics();
-
-    var renderer =  Viva.Graph.View.renderer(graph, {
-        layout: layout,
-        graphics: graphics
-        , container: document.body
-    });
-
-    var graphRect = layout.getGraphRect();
-    var graphSize = Math.min(graphRect.x2 - graphRect.x1, graphRect.y2 - graphRect.y1);
-    var screenSize = Math.min(document.body.clientWidth, document.body.clientHeight);
-    var desiredScale = screenSize / graphSize;
-    zoomOut(desiredScale, 1);
-    function zoomOut(desiredScale, currentScale) {
-        // zoom API in vivagraph 0.5.x is silly. There is no way to pass transform
-        // directly. Maybe it will be fixed in future, for now this is the best I could do:
-        if (desiredScale < currentScale) {
-            currentScale = renderer.zoomOut();
-            setTimeout(function () {
-                zoomOut(desiredScale, currentScale);
-            }, 16);
-        }
+        var communities = document.querySelector('#communities').value;
+        var nodesCount = document.querySelector('#nodesCount').value;
+        var bridgeCount = document.querySelector('#bridgeCount').value;
+        var isAtlas = document.querySelector('#force').checked;
+        this.querySelector('[type=submit]').isDisabled = true;
+        rendererSstate = init(communities, nodesCount, bridgeCount, isAtlas);
+        e.preventDefault();
     }
 
-    renderer.run(Infinity);
+}
 
+
+function init(communities, nodesCount, bridgeCount, force2) {
+    communities = _.range(communities);
+    var nodes = [];
+    var graph = Viva.Graph.graph();
+
+    console.time('build graph');
+    for (var i = 0, c; i < nodesCount; i++) {
+        c = _.random(communities.length - 1);
+        graph.addLink('n' + i, 'community' + c);
+        nodes[i] = c;
+    }
+    for (var j = 0, n, cs; j < bridgeCount; j++) {
+        n = _.random(nodesCount - 1);
+        cs = _.difference(communities, [nodes[n]]);
+        graph.addLink('n' + n, 'community' + cs[_.random(cs.length)]);
+    }
+    console.time('build graphEnd');
+
+
+    console.time('agm');
+    graph = agm(graph, {
+        coefficient: 0.3,
+        scale: 1
+    });
+    console.time('agm');
+
+
+    var layout;
+    if (force2)
+        layout = ForceAtlas2(graph, {
+            gravity: 1,
+            linLogMode: false,
+            strongGravityMode: false,
+            slowDown: 1,
+            outboundAttractionDistribution: false,
+            iterationsPerRender: 1,
+            barnesHutOptimize: false,
+            barnesHutTheta: 0.5,
+            worker: true
+        });
+
+    else
+        layout = Viva.Graph.Layout.forceDirected(graph, {
+            springLength: 30,
+            springCoeff: 0.0008,
+            dragCoeff: 0.01,
+            gravity: -1.2,
+            theta: 1
+        });
+
+    var graphics = Viva.Graph.View.webglGraphics()
+        , squareNode = Viva.Graph.View.webglSquare;
+
+    graphics
+        .node(function (node) {
+            return squareNode(
+                15
+                , Number(("0x" + colors[nodes[node.id.slice(1)]].slice(1) + "FF"))
+            );
+        });
+
+    var renderer = Viva.Graph.View.renderer(graph, {
+        renderLinks: false,
+        layout: layout,
+        graphics: graphics
+        , container: document.querySelector('#cont')
+    });
+
+
+    renderer.run(Infinity);
+    return renderer;
 }
 
 window.addEventListener('load', onLoad);
